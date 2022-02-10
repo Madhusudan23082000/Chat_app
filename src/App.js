2267f0905c07bd1c65949df996fa7d1071d52ac4
Madhusudan23082000 <73375985+Madhusudan23082000@users.noreply.github.com>
2022-02-10 22:59:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+function App() {
+  return (
+    <ChatEngine
+      publicKey='
+      68be617a-fc53-4799-8bb4-309b7f26180f'
+      userName='Naman'
+      userPassword='Shivparvati@1'
+      height='100vh'
+			userSecret='Shivparvati@1'
+			projectID='68be617a-fc53-4799-8bb4-309b7f26180f'
+    />
+  );
+}
+export default App;
